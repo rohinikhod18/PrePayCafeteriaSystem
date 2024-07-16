8fba952a96c433f156b4a819a41ae85ab69f7a9e
Rohini Khod <rohini.khod@bnt.local>
2024-07-16 13:05:12 +0530

    Add new files in CafeteriaSystem directory

---
+++ CafeteriaSystem/src/main/java/com/kpit/cps/CafeteriaSystemApplication.java
@@ -0,0 +1,14 @@
+package com.kpit.cps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeteriaSystemApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Cafeteria Project");
+		SpringApplication.run(CafeteriaSystemApplication.class, args);
+	}
+
+}
---
+++ CafeteriaSystem/src/test/java/com/kpit/cps/CafeteriaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpit.cps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeteriaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
